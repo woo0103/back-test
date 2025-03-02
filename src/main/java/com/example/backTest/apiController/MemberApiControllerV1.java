08d10ad6aa295d3e448ccb26fe44755c4bfe7b16
wooooo <playjiwoo@naver.com>
2025-03-02 15:11:10 +0900

    feat: 회원 ID 조회 시 존재하지 않는 경우 404 에러 처리 추가

--- src/main/java/com/example/backTest/apiController/MemberApiControllerV1.java
+++ src/main/java/com/example/backTest/apiController/MemberApiControllerV1.java
@@ -2,6 +2,7 @@ package com.example.backTest.apiController;
 
 import com.example.backTest.domain.Member;
 import com.example.backTest.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -23,8 +24,12 @@ public class MemberApiControllerV1 {
 
     // 특정 회원 조회
     @GetMapping("/members/{id}")
-    public Member findMember(@PathVariable("id") Long id) {
-        return memberRepository.findOne(id)  ;
+    public Member findMember(@PathVariable("id") Long id, HttpServletResponse response) {
+        Member member = memberRepository.findOne(id);
+        if (member == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+        return member;
     }
 
     // 전체 회원 조회
